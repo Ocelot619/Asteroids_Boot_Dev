8002a5d78b9debface49e246bfe4c4c5e2f3f6ba
Ocelot619 <konradc699@gmail.com>
2026-03-13 14:33:10 +0100

    poddaje sie

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 import pygame
-from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-from circleshape import *
 from player import Player
 
+
 def main():
-    print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
-    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    
-    # Tworzymy grupy przed stworzeniem Playera
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    #  Dodajemy Player do tych grup przed stworzeniem instancji
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
 
-    # Tworzymy instancję Player
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    running = True
-    while running:
+    dt = 0
+
+    while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
-        dt = clock.tick(60) / 1000
-
-        # Aktualizacja wszystkich obiektów w grupie updatable
         updatable.update(dt)
 
-        #Rysowanie wszystkich obiektów w grupie drawable
-        screen.fill((0, 0, 0))
-        for obj in drawable:
-            obj.draw(screen, "white", obj.points, LINE_WIDTH)
+        screen.fill("black")
 
-        pygame.display.flip()  # zawsze na końcu
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
